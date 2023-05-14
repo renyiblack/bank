8c49e8f942f36b9c7b58130801172246853bf3e2
victor morais <victorangelograca@hotmail.com>
2023-05-14 18:37:47 -0300

    chore: improve account storage
    added support for search in O(1) and removed id from account, the number is now the id

--- back/repositories/account.py
+++ back/repositories/account.py
@@ -1,23 +1,25 @@
+import typing
+
 from entities.account import Account
 
 
 class AccountRepository:
-    __accounts: []
+    __accounts: typing.Dict[int, Account]
 
     def __init__(self):
-        self.__accounts = []
+        self.__accounts = {}
 
     def add_account(self, acc: int):
-        self.__accounts.append(Account(len(self.__accounts), acc))
-        print(self.__accounts[0])
+        self.__accounts[acc] = Account(acc)
 
     def get_account_by_number(self, number: int) -> Account:
-        for acc in self.__accounts:
-            account: Account = acc
-            if account.number == number:
-                return account
-
-        raise Exception("account not found")
+        try:
+            return self.__accounts[number]
+        except:
+            raise Exception("account not found")
 
     def update_account(self, acc: Account):
-        self.__accounts[acc.id] = acc
+        try:
+            self.__accounts[acc.number] = acc
+        except:
+            raise Exception("account not found")
