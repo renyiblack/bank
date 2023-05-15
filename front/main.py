bb540e5f2656f77d982e0bd69f3693a6cfb2f9cf
Joyce Beatriz <joycesilva@discente.ufg.br>
2023-05-14 22:09:58 -0300

    Update_create_account
    Se <Response [201]> aparece a mensagem de conta criada com sucesso.

--- front/main.py
+++ front/main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+baseURL = "http://localhost:5000"
+
+def showMenu():
+    print("+--------------------+")
+    print("|        MENU        |")
+    print("+--------------------+")
+    print("| 0 - Exit           |")
+    print("| 1 - Create Account |")
+    print("| 2 - Get Balance    |")
+    print("| 3 - Deposit        |")
+    print("| 4 - Debit          |")
+    print("| 5 - Transfer       |")
+    print("+--------------------+")
+
+def create_account(number):
+    data = {
+        "account_number": number
+    }
+    response = requests.post(baseURL + "/account", json=data)
+    if response.status_code == 201:
+        print("Account created successfully! Account number:", number)
+    else:
+        print("Failed to create account. Status:", response.status_code)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_balance(number):
+    response = requests.get(baseURL + f"/balance/{number}")
+    if(response.status_code == 200):
+        print("=> Account balance is " + response.text)
+    else:    
+        print("=> " + response.text)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    userInput = 0
+    while True:
+        showMenu()
+        userInput = int(input("=> Choose an option:"))
+        
+        match userInput:
+            case 0:
+                print("=> Exit!")
+                break
+            case 1:
+                account_number = int(input("=> Enter account number:"))
+                create_account(account_number)
+            case 2:
+                account_number = int(input("=> Enter account number:"))
+                get_balance(account_number)
+            case 3:
+                print("=> Depositing in the account.")
+            case 4:
+                print("=> Debiting from the account.")
+            case 5:
+                print("=> Transferring money.")
+            case _:
+                print("=> Invalid Option!")
