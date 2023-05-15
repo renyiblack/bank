8e1f825b93470b50291b5a56924d149c04b782bf
Joyce Beatriz <48606497+joycebeatriz@users.noreply.github.com>
2023-05-14 23:29:45 -0300

    Create_transfer (#10)
    Solicita o número de conta de origem, número de conta de destino e valor a ser transferido. Realiza o débito da conta de origem e o crédito na conta destino.

--- front/main.py
+++ front/main.py
@@ -18,12 +18,15 @@ def create_account(number):
     data = {
         "account_number": number
     }
-    response = requests.post(baseURL + "/account", json = data)
-    print(response)
+    response = requests.post(baseURL + "/account", json=data)
+    if response.status_code == 201:
+        print("Account created successfully! Account number:", number)
+    else:
+        print("Failed to create account. Status:", response.status_code)
 
 def get_balance(number):
     response = requests.get(baseURL + f"/balance/{number}")
-    if(response.status_code == 200):
+    if response.status_code == 200:
         print("=> Account balance is " + response.text)
     else:
         print("=> " + response.text)
@@ -33,20 +36,67 @@ def credit(account_number, value):
         "account_number": account_number,
         "transaction": value
     }
-    response = requests.put(baseURL + "/balance", json = data)
+    response = requests.put(baseURL + "/balance", json=data)
 
-    if(response.status_code == 204):
-        print("=> Account updated succefully!")
+    if response.status_code == 204:
+        print("=> Account updated successfully!")
     else:
         print("=> " + response.text)
 
+def debit(account_number, value):
+    data = {
+        "account_number": account_number,
+        "transaction": -value
+    }
+    response = requests.put(baseURL + "/balance", json=data)
+
+    if response.status_code == 204:
+        print("=> Account updated successfully!")
+    else:
+        print("=> " + response.text)
+
+def transfer(source_account, destination_account, value):
+    # Verifica o saldo da conta de origem
+    response = requests.get(baseURL + f"/balance/{source_account}")
+    if response.status_code != 200:
+        print("=> Failed to transfer. Source account not found.")
+        return
+
+    source_balance = float(response.text)
+    
+    # Verifica se há saldo suficiente na conta de origem
+    if source_balance < value:
+        print("=> Failed to transfer. Insufficient funds in the source account.")
+        return
+
+    # Realiza o débito da conta de origem
+    debit_data = {
+        "account_number": source_account,
+        "transaction": -value
+    }
+    response = requests.put(baseURL + "/balance", json=debit_data)
+    if response.status_code != 204:
+        print("=> Failed to transfer. Error debiting the source account.")
+        return
+
+    # Realiza o crédito na conta de destino
+    credit_data = {
+        "account_number": destination_account,
+        "transaction": value
+    }
+    response = requests.put(baseURL + "/balance", json=credit_data)
+    if response.status_code != 204:
+        print("=> Failed to transfer. Error crediting the destination account.")
+        return
+
+    print("=> Transfer successful!")
 
 if __name__ == '__main__':
     userInput = 0
     while True:
         showMenu()
         userInput = int(input("=> Choose an option:"))
-        
+
         match userInput:
             case 0:
                 print("=> Exit!")
@@ -62,8 +112,13 @@ if __name__ == '__main__':
                 value = int(input("=> Enter the amount to deposit:"))
                 credit(account_number, value)
             case 4:
-                print("=> Debiting from the account.")
+                account_number = int(input("=> Enter account number:"))
+                value = int(input("=> Enter the amount to debit:"))
+                debit(account_number, value)
             case 5:
-                print("=> Transferring money.")
+                source_account = int(input("=> Enter source account number:"))
+                destination_account = int(input("=> Enter destination account number:"))
+                value = int(input("=> Enter the amount to transfer:"))
+                transfer(source_account, destination_account, value)
             case _:
                 print("=> Invalid Option!")
