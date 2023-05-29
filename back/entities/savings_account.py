c18d573453b03447c03c52770cd3845d7012e8d7
Victor Ângelo <victorangelograca@hotmail.com>
2023-05-28 22:55:53 -0300

    feat: add initial value to savings account (#23)

--- back/entities/savings_account.py
+++ back/entities/savings_account.py
@@ -3,9 +3,9 @@ from entities.account import Account
 
 class SavingsAccount(Account):
 
-    #def __init__(self, acc: Account):
-    #    super().__init__(acc.number)
-    #    self.balance = acc.balance
+    def __init__(self, acc: Account, initial_value: float):
+        super().__init__(acc.number)
+        self.balance = initial_value
 
     def yield_interest(self, rate: float):
         self.balance += self.balance * (rate / 100)
